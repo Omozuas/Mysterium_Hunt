1806a54d782166a3ad5f2e852d10817e18574b14
omozuas <iyanuomozua@gmail.com>
2024-08-08 16:41:08 +0100

    new

--- lib/views/auth/log_in_screen.dart
+++ lib/views/auth/log_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:argame/profile_screen.dart';
 import 'package:argame/views/auth/forgot_password_screen.dart';
 import 'package:argame/views/auth/sign_up_screen.dart';
+import 'package:argame/views/menu_screen.dart';
 import 'package:argame/widgets/custom_full_button.dart';
 import 'package:argame/widgets/custom_password_field.dart';
 import 'package:argame/widgets/custom_textfield.dart';
@@ -19,130 +20,145 @@ class _LogInScreenState extends State<LogInScreen> {
   bool? checkBoxValue = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Text(
-                  'Log In!',
-                  style: GoogleFonts.inter(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Provide your credentials to access your saved progress and continue hunting!',
-                style: GoogleFonts.syne(
-                    fontSize: 18,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w400),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Email or username',
-                style: GoogleFonts.syne(
-                    color: const Color(0xff565050),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
-              ),
-              buildTextField(
-                  type: TextInputType.text, hintText: 'prodigy_rejoice'),
-              SizedBox(height: 10),
-              Text(
-                'Password',
-                style: GoogleFonts.syne(
-                    color: const Color(0xff565050),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
-              ),
-              const PasswordField(hintText: 'Enter Password'),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Checkbox(
-                          visualDensity:
-                              const VisualDensity(horizontal: -4, vertical: -4),
-                          side: const BorderSide(color: Color(0xffD8D8DD)),
-                          activeColor: Colors.brown,
-                          value: checkBoxValue,
-                          onChanged: (bool? newValue) {
-                            setState(() {
-                              checkBoxValue = newValue;
-                            });
-                          }),
-                      Text(
-                        'Remember me',
-                        style: GoogleFonts.poppins(
-                            fontSize: 12, fontWeight: FontWeight.w500),
-                      )
-                    ],
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.fitHeight,
+              image: AssetImage('assets/images/hunt.jpeg'))),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Text(
+                    'Log In!',
+                    style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (c) => ForgotPasswordScreen()));
-                    },
-                    child: Text(
-                      'Forgot Password?',
-                      style: GoogleFonts.poppins(
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Provide your credentials to access your saved progress and continue hunting!',
+                  style: GoogleFonts.syne(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Email or username',
+                  style: GoogleFonts.syne(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400),
+                ),
+                buildTextField(
+                    type: TextInputType.text, hintText: 'prodigy_rejoice'),
+                SizedBox(height: 10),
+                Text(
+                  'Password',
+                  style: GoogleFonts.syne(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400),
+                ),
+                const PasswordField(hintText: 'Enter Password'),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                            visualDensity: const VisualDensity(
+                                horizontal: -4, vertical: -4),
+                            side: const BorderSide(color: Color(0xffD8D8DD)),
+                            activeColor: Colors.brown,
+                            value: checkBoxValue,
+                            onChanged: (bool? newValue) {
+                              setState(() {
+                                checkBoxValue = newValue;
+                              });
+                            }),
+                        Text(
+                          'Remember me',
+                          style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (c) => ForgotPasswordScreen()));
+                      },
+                      child: Text(
+                        'Forgot Password?',
+                        style: GoogleFonts.poppins(
                           fontWeight: FontWeight.w500,
                           fontSize: 12,
-                          color: const Color(0xffD9291E)),
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 40),
-              FullButton(
-                text: 'Log Into Account',
-                width: double.infinity,
-                height: 44,
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ProfileScreen()));
-                },
-                color: Colors.white,
-              ),
-              SizedBox(height: 15),
-              Center(
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: 'Don’t have account? ',
-                          style: GoogleFonts.poppins(
+                  ],
+                ),
+                SizedBox(height: 40),
+                FullButton(
+                  text: 'Log Into Account',
+                  width: double.infinity,
+                  height: 44,
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MenuScreen()));
+                  },
+                  color: Colors.white,
+                ),
+                SizedBox(height: 15),
+                Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: 'Don’t have account? ',
+                            style: GoogleFonts.poppins(
                               fontSize: 14,
                               fontWeight: FontWeight.w400,
-                              color: const Color(0xff737380))),
-                      TextSpan(
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignUpScreen())),
-                        text: 'Sign Up',
-                        style: GoogleFonts.poppins(
+                              color: Colors.white,
+                            )),
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const SignUpScreen())),
+                          text: 'Sign Up',
+                          style: GoogleFonts.poppins(
                             fontWeight: FontWeight.w700,
                             fontSize: 14,
-                            color: const Color(0xffEC8305)),
-                      ),
-                    ],
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/views/auth/sign_up_screen.dart
+++ lib/views/auth/sign_up_screen.dart
@@ -12,135 +12,146 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: GestureDetector(
-        onTap: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => SignUpFeedbackScreen()));
-        },
-        child: Container(
-          height: 50,
-          width: 53,
-          decoration: const BoxDecoration(
-            color: Colors.black54,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(22),
-                topRight: Radius.circular(5),
-                bottomLeft: Radius.circular(5),
-                bottomRight: Radius.circular(9)),
-          ),
-          child: Icon(
-            Icons.arrow_forward,
-            color: Colors.white,
-            size: 25,
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('assets/images/hunt.jpeg'))),
+      child: Scaffold(
+        floatingActionButton: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SignUpFeedbackScreen()));
+          },
+          child: Container(
+            height: 50,
+            width: 53,
+            decoration: const BoxDecoration(
+              color: Colors.black54,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(22),
+                  topRight: Radius.circular(5),
+                  bottomLeft: Radius.circular(5),
+                  bottomRight: Radius.circular(9)),
+            ),
+            child: Icon(
+              Icons.arrow_forward,
+              color: Colors.white,
+              size: 25,
+            ),
           ),
         ),
-      ),
-      backgroundColor: Color(0xffFFFFFF),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Text(
-                  'Sign Up!',
-                  style: GoogleFonts.inter(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Fill in the details correctly to register an account successfully',
-                style: GoogleFonts.syne(
-                    fontSize: 18,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w400),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Username',
-                style: GoogleFonts.syne(
-                    color: const Color(0xff565050),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
-              ),
-              buildTextField(
-                  type: TextInputType.text, hintText: 'prodigy_rejoice'),
-              SizedBox(height: 10),
-              Text(
-                'Email',
-                style: GoogleFonts.syne(
-                    color: const Color(0xff565050),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
-              ),
-              buildTextField(
-                  type: TextInputType.emailAddress,
-                  hintText: 'prodigy@gmail.com'),
-              SizedBox(height: 10),
-              Text(
-                'Password',
-                style: GoogleFonts.syne(
-                    color: const Color(0xff565050),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
-              ),
-              const PasswordField(hintText: 'Enter Password'),
-              SizedBox(height: 10),
-              Text(
-                'Confirm Password',
-                style: GoogleFonts.syne(
-                    color: const Color(0xff565050),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
-              ),
-              const PasswordField(hintText: 'Enter Password'),
-              SizedBox(height: 40),
-              FullButton(
-                text: 'Create Account',
-                width: double.infinity,
-                height: 44,
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SignUpFeedbackScreen()));
-                },
-                color: Colors.white,
-              ),
-              SizedBox(height: 15),
-              Center(
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: 'Already have a account? ',
-                          style: GoogleFonts.poppins(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: const Color(0xff737380))),
-                      TextSpan(
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LogInScreen())),
-                        text: 'Login',
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 14,
-                            color: const Color(0xffEC8305)),
-                      ),
-                    ],
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Text(
+                    'Sign Up!',
+                    style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                Text(
+                  'Fill in the details correctly to register an account successfully',
+                  style: GoogleFonts.syne(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Username',
+                  style: GoogleFonts.syne(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400),
+                ),
+                buildTextField(
+                    type: TextInputType.text, hintText: 'prodigy_rejoice'),
+                SizedBox(height: 10),
+                Text(
+                  'Email',
+                  style: GoogleFonts.syne(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400),
+                ),
+                buildTextField(
+                    type: TextInputType.emailAddress,
+                    hintText: 'prodigy@gmail.com'),
+                SizedBox(height: 10),
+                Text(
+                  'Password',
+                  style: GoogleFonts.syne(
+                      color: const Color(0xff565050),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400),
+                ),
+                const PasswordField(hintText: 'Enter Password'),
+                SizedBox(height: 10),
+                Text(
+                  'Confirm Password',
+                  style: GoogleFonts.syne(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400),
+                ),
+                const PasswordField(hintText: 'Enter Password'),
+                SizedBox(height: 40),
+                FullButton(
+                  text: 'Create Account',
+                  width: double.infinity,
+                  height: 44,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SignUpFeedbackScreen()));
+                  },
+                  color: Colors.white,
+                ),
+                SizedBox(height: 15),
+                Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: 'Already have a account? ',
+                            style: GoogleFonts.poppins(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            )),
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LogInScreen())),
+                          text: 'Login',
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
